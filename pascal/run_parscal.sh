03aa59db198191132373002c9cf9ef79e7ef1597
samuel-cavalcanti <scavalcanti111@gmail.com>
2022-06-10 18:02:39 -0300

    reconfigurado pascalanalyzer

--- pascal/run_parscal.sh
+++ pascal/run_parscal.sh
@@ -2,7 +2,9 @@
 
 # Execute esse script depois de instanciar o pascalanalizer
 
-NTHREADS=8 # tenho 8 threads
+NTHREADS="__nt__" 
+# segundo frankson, tenho que passar esse parâmetro  __nt__
+# para os cores.
 
 # simsmall
 #run_args="-ns 16 -sm 10000 -nt ${NTHREADS}"
@@ -28,5 +30,7 @@ native="-ns 128 -sm 1000000 -nt ${NTHREADS}"
 # -sm == NUM_TRIALS número de tentativas por simulação
 # -nt == nThreads  número de Threads
 
+# tenho 8 threads, portanto
+MY_CORES=8;
 
-pascalanalyzer --ipts "$simsmall,$simmedium,$simlarge,$native" "./swaptions" -o "swaptions.json"
+pascalanalyzer -c 1:$MY_CORES --ipts "$simsmall,$simmedium,$simlarge,$native" "./swaptions" -o "swaptions.json"
